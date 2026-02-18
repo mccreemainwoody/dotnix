ede5c3a29f7cf8a9d10b141f8dd7de03cdb6552e
Clément Metz <metz.clement@gmail.com>
2026-02-19 00:47:55 +0100

    feat: added alsa audio support and pavu

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,10 @@
     services.pipewire = {
         enable = true;
         pulse.enable = true;
+        alsa = {
+            enable = true;
+            support32Bit = true;
+        };
     };
     services.xserver = {
         enable = true;
